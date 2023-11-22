5411fc6f905c83e3be4b69a9da0fe2cc3998d1a1
Shwetha <shwethapateel@gmail.com>
2023-11-22 18:04:58 +0530

    Jwt updation

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -23,4 +23,93 @@ const postBlog = async (req,res)=>{
        }); 
     }
 }
-module.exports = postBlog;
+
+const getByAuthor = async (req,res)=>{
+    try {
+        let user = req.user;
+        const authorBlogs = await blogs.find({author : user._id})
+        res.status(201).json({
+            status : 'success',
+            data : {
+                authorBlogs
+            }
+        })
+    } catch (error) {
+       res.status(400).json({
+         status: "fail",
+         message : error.message
+       }); 
+    }
+}
+
+const getBlogs = async (req,res) =>{
+    try {
+        let newBlogs = await blogs.find()
+        res.status(200).json({
+            status : 'success',
+            data : {
+                newBlogs
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            status : 'fail',
+            message : error.message
+        })
+    }
+}
+
+const getBlog = async (req,res) =>{
+    try {
+        let newBlog = await blogs.findById(req.params.id)
+        res.status(200).json({
+            status : "success",
+            data : {
+                newBlog
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
+}
+
+const updateBlog = async (req,res) =>{
+    try {
+        const {id} = req.params
+        const updatedBlog = await blogs.findByIdAndUpdate(id, req.body,{new : true})
+        res.status(200).json({
+            status : "success",
+            data : {
+                updatedBlog
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
+}
+
+const deleteBlog = async (req,res) =>{
+    try {
+        let user = req.user
+        await blogs.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            status : 'success',
+            data : null
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
+}
+
+module.exports = {
+  postBlog, getBlogs, getBlog, updateBlog, deleteBlog, getByAuthor
+}
--- routers/BlogRoutes.js
+++ routers/BlogRoutes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const auth = require('../middlewares/authMiddleware.js')
-const postBlog = require('../controllers/blogControllers.js')
+const {postBlog, getByAuthor, getBlog, getBlogs, updateBlog, deleteBlog} = require('../controllers/blogControllers.js')
 
 router.post("/", auth, postBlog);
-// router.get('/', auth, getBlogs)
-// router.get("/:id", auth, getBlog);
-// router.patch("/:id", auth, updateBlog);
-// router.delete("/:id", auth, deleteBlog);
+router.get('/', auth, getBlogs)
+router.get("/author", auth, getByAuthor);
+router.get("/:id", auth, getBlog);
+router.patch("/:id", auth, updateBlog);
+router.delete("/:id", auth, deleteBlog);
 
 module.exports = router;
