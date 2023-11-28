03dff84cb87e77934e62c5dc5656472ab48a2ddb
Shwetha <shwethapateel@gmail.com>
2023-11-28 18:16:50 +0530

    Implement Custom Error

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express = require("express")
-const authRoutes = require("./routers/userRouters.js")
+const express = require("express");
+const authRoutes = require("./routers/userRouters.js");
 const BlogRouter = require("./routers/BlogRoutes.js");
 const globalErrorHandler = require("./controllers/globalErrorHandler.js");
+const CustomError = require("./utils/CustomError.js").default;
 const app = express();
 
 app.use(express.json());
-app.use("/app/v1/users", authRoutes)
-app.use("/app/v1/blogs", BlogRouter)
+app.use("/app/v1/users", authRoutes);
+app.use("/app/v1/blogs", BlogRouter);
 
 app.all("*", (req, res, next) => {
   //   res.status(404).json({ status: "fail", message: "Page not found" })
-    let err = new Error("Page Not Found")
-    err.statusCode = 404
-    err.status = "fail"
-    next(err)
-})
+  // let err = new Error("Page Not Found")
+  // err.statusCode = 404
+  // err.status = "fail"
+  // next(err)
+  let err = new CustomError(404, "Page Not Found");
+  next(err);
+});
 
 /////Global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ utils/CustomError.js
@@ -0,0 +1,10 @@
+class CustomError extends Error{
+    constructor(statusCode, message){
+        super(message)
+        this.statusCode = statusCode
+        this.status = statusCode >= 400 && statusCode <= 500 ? "Fail" : "error"
+
+        Error.captureStackTrace(this, this.constructor)  /////this => pointing to Error & this.contrictor => pointing to CustomError
+    }
+}
+module.exports = CustomError
